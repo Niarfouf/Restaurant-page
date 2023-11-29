51a12a1394399a7dfdc7fa2489803c204f7ccdf4
Niarfouf <tniarfeix@gmail.com>
2023-11-29 14:53:18 +0100

    Add: html template with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,7 +8,13 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
